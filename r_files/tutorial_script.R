07bc6fff7fab63eb7e243ab1f73fd5f14a44f49a
giadaleone99 <giada.leone99@outlook.com>
2021-12-02 16:38:34 +0100

    Giada structures script and finishes code for leaflet map

--- r_files/tutorial_script.R
+++ r_files/tutorial_script.R
@@ -9,16 +9,17 @@ rm(list= ls())
 # Libraries 
 library(tidyverse)
 library(janitor)
+library(leaflet)
 library(ggthemes)
 library(sf)
 library(mapview)
 
+# IMPORT AND CLEAN DATA ----
 # Load the data
 beaches <- read.csv("data/beachwatch_data.csv", header = TRUE)
 str(beaches)
 
 # Preparing the data 
-
 beaches <- beaches %>% clean_names()   # cleaning data frame column names
 beaches <- select(beaches,-seq(12, 109), -seq(112, 170))  # deleting unused columns
 beaches <- drop_na(beaches)  # dropping columns which have NA values
@@ -49,6 +50,8 @@ ggplot(beaches, aes(x = beach_longitude,
    geom_point(size = 1) +
    theme_map() 
    
+
+
 
 # Changing map background
 ggplot(beaches, aes(x = beach_longitude,
@@ -105,119 +108,94 @@ ggplot(beaches, aes(x = beach_longitude,
 ggsave("outputs/beaches_map.png", width = 7, height = 5)
 
 
+# CALCULATIONS, WRANGLING AND INTERACTIVE MAP ----
 
-# Calculating beach cigarette litter density in 2018
+# Calculating beach average cigarette litter density between 2008 - 2018
 # Group by beach ID, then add cig stubbs and packets then divide this 
-# by the number of surveys*100m? 
+# by the number of surveys for that beach to obtain an average 
+# Then divide by 100m to obtain average density of CRL 
 
 # Here we are going to create 2 separate data frames, so we can learn how to 
 # combine them by a common column using left_join()
 
-# Filtering for 2018 surveys only
-surveys_2018 <- beaches %>% 
-   filter(year %in% 2018) %>% 
-   group_by(beach_id) %>% 
-   summarise(total_crl = sum(paper_cardboard_cigarette_packets, paper_cardboard_cigarette_stubs)) %>%
-ungroup() 
-
-# So we have the total cigarette related litter counts for all beaches in 2018
-# Now we need to divide that by the number of surveys done for each beach ID
-
-surveys_2018 <- beaches %>% 
-   filter(year %in% 2018) %>% 
-   group_by(beach_id) %>% 
-   count(beach_id) %>% 
-ungroup()
-
-# Combining the datasets
-combo <- left_join(surveys_2018, num_surveys)
-
-# Now let's divide the total crl by the number of surveys done for each beach
-combo <- combo %>% 
-   mutate(average_crl = round(total_crl/n),  # Rounding whole number
-          crl_density = round(average_crl/100, 2))  # Rounding to 2 decimal places
-
-## What if we do this for all the years? 
-
+# Creating a dataframe with a column for total CRL
 surveys <- beaches %>% 
    group_by(beach_id) %>% 
-   summarise(total_crl = sum(paper_cardboard_cigarette_packets, paper_cardboard_cigarette_stubs)) %>%
-   mutate()
+   summarise(total_crl = sum(paper_cardboard_cigarette_packets, paper_cardboard_cigarette_stubs)) %>% 
    ungroup() 
 
+# Dataframe for number of surveys done for each beach (2008-2018)
+num_surveys <- beaches %>% 
+   group_by(beach_id) %>% 
+   count(beach_id) %>% 
+   ungroup()
 
-... 
+# Here we are merging the two dataframes using the function left_join()
+# Since the two dataframes have a column name in common, they are joined by that column
+combo <- left_join(surveys, num_surveys)
+
+# Now let's create columns for average CRL and density
+# Divide the total CRL by the number of surveys done for each beach --> average
+# Divide average CRL by 100 to obtain CRL density/100m
+combo <- combo %>% 
+   mutate(average_crl = round(total_crl/n),  # Rounding whole number
+          crl_density = round(average_crl/100, 2))
    
-   
-
-
+# Creating a new dataframe with the extra information we calculated
+beaches2 <- left_join(beaches, combo)
 
 
 
 ## Creating an interactive map using the leaflet package ----
-# Learining how to use leaflet
-install.packages("leaflet")
-library(leaflet)
+# Learning how to use leaflet
 
 leaflet() %>% addTiles() %>% 
-   addCircleMarkers(data = beaches, lat = ~beach_latitude, lng = ~beach_longitude)
-
-
+   addCircleMarkers(data = beaches2, lat = ~beach_latitude, lng = ~beach_longitude)
 
 # Adjusting point size
 leaflet() %>% 
    addTiles() %>% 
-   addCircleMarkers(data = beaches, lat = ~beach_latitude, lng = ~beach_longitude, radius = 1)
-
+   addCircleMarkers(data = beaches2, lat = ~beach_latitude, lng = ~beach_longitude, radius = 1)
 
 # Making the points interactive
-# Adding data to display when points are clicked
-
-
-# Going to create a new column for the popup information in the beaches dataset 
-# Possibly add density of cigarette related litter?
-
-
-beaches <- beaches %>% 
-   mutate(popup_info = paste(beach_name, "<br/>", beach_county, "<br/>", beach_region))
-
+# Adding column to display data when points are clicked
+beaches2 <- beaches2 %>% 
+   mutate(popup_info = paste(beach_name, "<br/>", beach_county, "<br/>", beach_region, "<br/>", "average CRL/100m: ", average_crl ))
 
 # Adding the popup information to the map 
 leaflet() %>% 
    addTiles() %>% 
-   addCircleMarkers(data = beaches, 
+   addCircleMarkers(data = beaches2, 
                     lat = ~beach_latitude, 
                     lng = ~beach_longitude, radius = 1,
                     popup = ~popup_info)
 
 
-# Create a palette to colour points by cigarette related litter density
-
-colours
-
-
+# Colour points based on average CRL density on the beach
+colours <- c("#005AB5", "#DC3200")  # Lower and upper colours of palette
+                                    # These are colourblind friendly
 
 
+# Creating a palette using the previously created colours object
+# Palette is created for average crl_density so that beaches with high 
+# CRL densities will be coloured in red and beaches with lower densities in blue
+pal <- colorFactor(colours, beaches2$crl_density)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+# Adding the coloured points to the map
+leaflet() %>% 
+   addTiles() %>% 
+   addCircleMarkers(data = beaches2, 
+                    lat = ~beach_latitude, 
+                    lng = ~beach_longitude, radius = 1,
+                    popup = ~popup_info,
+                    color = ~pal(crl_density)) 
+   
 
 
 
 
-# sf and mapview packages
+# sf and mapview packages ----
 
 # Spatial analysis
 beaches_obj <- st_as_sf(beaches, coords =  c('beach_longitude', 'beach_latitude'), crs = 4326)
